1341af89f87696ba74e9f47b8d814871ad394836
Joao-Inacio <joaoinacio206@protonmail.com>
2023-11-25 09:30:07 -0300

    Tipo str (string) - Introdução aos tipos de dados

---
+++ Python-Basico/0.3-Aula.py
@@ -0,0 +1,16 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte 
+str - > string - > texto
+Strings são textos que estão dentro de aspas
+"""
+# Aspas simples
+print('Hello')
+# Aspas duplas
+print("Hello")
+# Escape
+print("Hello \"World\"")
+# r
+print(r"Hello \"World\"")
+# Aspas simples dentro de aspas duplas
+print("Hello 'World'")
