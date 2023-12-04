1e9117cefcb502dfc9038f00030b8276761e7879
Joao-Inacio <joaoinacio206@protonmail.com>
2023-12-04 14:25:39 -0300

    Exercício de programação - Cálculo do IMC (Índice de Massa Corpórea) + Ellipsis

---
+++ Python-Basico/1.2-Exercicio.py
@@ -0,0 +1,8 @@
+"""
+Exercício de programação - Cálculo do IMC (Índice de Massa Corpórea) + Ellipsis
+"""
+nome = 'João'
+altura = 1.78
+peso = 90
+imc = peso / (altura ** 2)
+print(f'{nome} tem {altura} de altura, \npesa {peso} quilos e seu IMC é \n{imc}')
