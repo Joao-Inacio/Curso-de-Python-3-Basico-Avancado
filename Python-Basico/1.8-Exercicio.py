4872ea94043ee4ee11891d324bfd47337c82e1e0
Joao-Inacio <joaoinacio206@protonmail.com>
2023-12-12 08:56:28 -0300

    Exercício

---
+++ Python-Basico/1.8-Exercicio.py
@@ -0,0 +1,48 @@
+"""
+Faça um programa que peça ao usuário para digitar um número inteiro,
+informe se este número é par ou ímpar. Caso o usuário não digite um número
+inteiro, informe que não é um número inteiro.
+"""
+numero = input("Digite um número inteiro: ")
+
+try:
+    numero = int(numero)
+    if numero % 2 == 0:
+        print("O número é par")
+    else:
+        print("O número é ímpar")
+except:
+    print("Não é um número inteiro")
+
+"""
+Faça um programa que pergunte a hora ao usuário e, baseando-se no horário 
+descrito, exiba a saudação apropriada. Ex. 
+Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23.
+"""
+
+hora = input("Digite a hora: ")
+
+try:
+    hora = int(hora)
+    if hora >= 0 and hora <= 11:
+        print("Bom dia")
+    elif hora >= 12 and hora <= 17:
+        print("Boa tarde")
+    elif hora >= 18 and hora <= 23:
+        print("Boa noite")
+except: 
+    print("Hora inválida")
+
+"""
+Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou 
+menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva 
+"Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande". 
+"""
+nome = input("Digite seu nome: ")
+
+if len(nome) <= 4:
+    print("Seu nome é curto")
+elif len(nome) >= 5 and len(nome) <= 6:
+    print("Seu nome é normal")
+else:
+    print("Seu nome é muito grande")
