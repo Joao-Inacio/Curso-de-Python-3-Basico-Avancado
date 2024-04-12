895bf08c956d3a85b81ec7e8e439aa179509deda
Joao-Inacio <joaoinacio206@protonmail.com>
2024-04-12 10:38:52 -0300

    Exercício com variáveis e tipos de dados

---
+++ Python-Basico/0.09-Aula.py
@@ -0,0 +1,19 @@
+"""
+Introdução aos operadores aritméticos (matemática)
+"""
+adicao = 10 + 10
+subtracao = 10 - 5
+multiplicacao = 10 * 10
+divisao = 10 / 2
+divisao_inteira = 10 // 3
+exponeciacao = 2 ** 10
+modulo = 55 % 2
+
+
+print('Adição:',adicao)
+print('Subtração:',subtracao)
+print('Multiplicação:',multiplicacao)
+print('Divisão:',divisao)
+print('Divisão Inteira:',divisao_inteira)
+print('Exponenciação:',exponeciacao)
+print('Resto da Divisão:',modulo)
