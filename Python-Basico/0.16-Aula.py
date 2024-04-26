004ba838b4dabf38f702351c166a171c904033da
Joao-Inacio <joaoinacio206@protonmail.com>
2024-04-26 09:37:30 -0300

    Introdução aos blocos de código + if / elif / else (condicionais)

---
+++ Python-Basico/0.16-Aula.py
@@ -0,0 +1,12 @@
+"Introdução aos blocos de código + if / elif / else (condicionais)"
+# if / elif / else (condicionais)
+# se / se não se / se não
+
+idade = int(input("Digite sua idade: "))
+
+if idade > 18:
+    print("Maior de idade")
+elif idade == 18:
+    print("Tem 18 anos")
+else:
+    print("Menor de idade")
