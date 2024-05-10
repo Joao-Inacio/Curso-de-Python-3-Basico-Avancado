ff198546d0dd3a0f0f1429537424d15d6c2e9699
Joao-Inacio <joaoinacio206@protonmail.com>
2024-05-10 10:56:20 -0300

    tipos built-in, documentação, tipo imutáveis, métodos de string

---
+++ Python-Basico/0.33-Aula.py
@@ -0,0 +1,9 @@
+"""
+Imutáveis que vimos: str, int, float, bool
+"""
+string = 'João Inácio'
+outra_variavel = f'{string[:3]}...'
+print(outra_variavel)
+print(string.capitalize())
+print(string.upper())
+print(string.zfill(30))
