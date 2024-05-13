3282085a025a34c3f963109aa1493e8c3ede0675
Joao-Inacio <joaoinacio206@protonmail.com>
2024-05-13 09:24:30 -0300

    2. Entrada e Saída:

---
+++ exercicios/entrada_e_saida.py
@@ -0,0 +1,12 @@
+"""
+2. Entrada e Saída:
+"""
+nome = str(input("Seu nome: "))
+idade = int(input("Sua idade: "))
+altura = float(input("Sua altura: "))
+estuda = bool(input("Você estuda? [True/False]: "))
+
+print(f'Seu nome é {nome}')
+print(f'Você tem {idade} anos')
+print(f'Sua altura é {altura}m')
+print(f'Estuda {estuda}')
