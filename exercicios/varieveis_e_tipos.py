3c8460a18725ffb0e002bbd4bb3c3fb9a64a3b7e
Joao-Inacio <joaoinacio206@protonmail.com>
2024-05-13 09:15:58 -0300

    Exercícios Básicos de Python 3 1. Variáveis e Tipos de Dados:

---
+++ exercicios/varieveis_e_tipos.py
@@ -0,0 +1,22 @@
+"""
+1. Variáveis e Tipos de Dados:
+"""
+string = 'João'
+inteiro = 25
+reais = 1.78
+estuda = True
+
+mais_um_ano = inteiro + 1
+menos_um_ano = inteiro - 1
+dobro_de_um_ano = inteiro * 2
+dividido_por_cinco_anos = 25 / 5
+
+print(mais_um_ano)
+print(menos_um_ano)
+print(dobro_de_um_ano)
+print(dividido_por_cinco_anos)
+
+peso = "75"
+peso = int(peso)
+print(
+    f'Nome: {string}, idade: {inteiro}, altura: {reais}, Estuda: {estuda}, Peso: {peso}')
