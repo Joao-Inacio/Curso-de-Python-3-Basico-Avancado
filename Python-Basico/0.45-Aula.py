20c71a0209345b3ac216d5dee0c015c68c88aa66
Joao-Inacio <joaoinacio206@protonmail.com>
2024-05-31 13:48:14 -0300

    Como o for funciona por baixo dos panos? (next, iter, iterável e iterador)

---
+++ Python-Basico/0.45-Aula.py
@@ -0,0 +1,15 @@
+"""
+Iterável -> str, range, etc (__iter__)
+Iterador -> quem sabe entregar um valor por vez
+next -> me entregue o próximo valor
+iter -> me entregue seu iterador
+"""
+# for letra in texto:
+#     print(letra)
+
+texto = 'Python'
+texto_iteravel = iter(texto)
+
+print(next(texto_iteravel))
+print(next(texto_iteravel))
+print(next(texto_iteravel))
