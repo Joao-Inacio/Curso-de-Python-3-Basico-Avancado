30a8807f033840ad2e9980440512dd06c698cc8c
Joao-Inacio <joaoinacio206@protonmail.com>
2024-05-31 13:39:36 -0300

    range + for para usar intervalos de números

---
+++ Python-Basico/0.44-Aula.py
@@ -0,0 +1,14 @@
+"""
+For + Range
+range -> range(start, stop, step)
+"""
+
+for i in range(0, 10, 2):
+    print(i)
+
+print("=" * 30)
+
+for i in range(10, 0, -1):
+    print(i)
+
+print("=" * 30)
