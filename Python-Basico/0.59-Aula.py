38224e7d9305bb4324124e010f3fe688ab437992
Joao-Inacio <joaoinacio206@protonmail.com>
2024-07-03 13:55:58 -0300

    Imprecisão dos números de ponto flutuante + round e decimal.Decimal

---
+++ Python-Basico/0.59-Aula.py
@@ -0,0 +1,18 @@
+"""
+Imprecisão de ponto flutuante
+Double-precision floating-point format IEEE 754
+https://en.wikipedia.org/wiki/Double-precision_floating-point_format
+https://docs.python.org/pt-br/3/tutorial/floatingpoint.html
+"""
+
+from decimal import Decimal
+from math import ceil
+
+n1 = 0.1
+n2 = 0.7
+n3 = n1 + n2
+print(n3)
+
+print(ceil(n3))
+print(round(n3, 2))
+print(Decimal(0.1 + 0.7))
