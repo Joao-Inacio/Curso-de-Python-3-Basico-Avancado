b0f6d562e1085944e914990dfbf98d67c66909de
Joao-Inacio <joaoinacio206@protonmail.com>
2024-08-02 13:25:43 -0300

    Valores padrão para parâmetros em funções Python + NoneType e None

---
+++ Python-Intermediario/0.68-Aula.py
@@ -0,0 +1,20 @@
+"""
+Valores padrão para parâmetros
+Ao definir uma função, os parâmetros podem
+ter valores padrão. Caso o valor não seja
+enviado para o parâmetro, o valor padrão será
+usado.
+Refatorar: editar o seu código.
+"""
+
+def soma(x, y, z=None):
+    if z is not None:
+        print(x, y, z, x + y + z)
+    else:
+        print(x + y)
+Valores padrão para parâmetros em funções Python + NoneType e None
+
+
+soma(2, 3)  # 5
+soma(4, 5)  # 9
+soma(1, 2, 3)  # 6
