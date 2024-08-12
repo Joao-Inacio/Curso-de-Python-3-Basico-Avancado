a9acddba16022981c422b6e83d4c91cb0f57890b
Joao-Inacio <joaoinacio206@protonmail.com>
2024-08-12 14:26:47 -0300

    (Parte 1) *args para quantidade de argumentos não nomeados variáveis

---
+++ Python-Intermediario/0.71-Aula.py
@@ -0,0 +1,13 @@
+"""
+args - Argumentos não nomeados
+* - *args (empacotamento e desempacotamento)
+"""
+
+def soma(*args):
+    total = 0
+    for numero in args:
+        total += numero
+    return total
+
+soma_1_2_3 = soma(1, 2, 3, 4, 5, 6, 7, 8, 9)
+print(soma_1_2_3)
