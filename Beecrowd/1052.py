d930f8514ee56d69248dda2b70ef44d457cb6d32
Joao-Inacio <joaoinacio206@protonmail.com>
2024-09-20 09:13:19 -0300

    Desafio 1052 Beecrowd

---
+++ Beecrowd/1052.py
@@ -0,0 +1,6 @@
+mes = int(input())
+meses = [
+    ' ', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 
+    'August', 'September', 'October', 'November', 'December'
+]
+print(meses[mes])
