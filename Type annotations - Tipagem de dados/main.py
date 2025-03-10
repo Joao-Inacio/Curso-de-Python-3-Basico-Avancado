232ad4babeb881b9508ee9fd94ad48749a45d208
Joao-Inacio <joaoinacio206@protonmail.com>
2025-03-10 18:03:51 -0300

    Adiciona exemplos de anotações de tipo em Python

---
+++ Type annotations - Tipagem de dados/main.py
@@ -0,0 +1,11 @@
+
+texto: str = 'Uma palavra'
+numero: int = 1234
+flutuante: float = 3.5
+boleano: bool = True
+
+um_set: set = {1, 2, 3}
+uma_lista: list = []
+uma_tupla: tuple = 1, 2, 3
+um_dic: dict = {}
+
