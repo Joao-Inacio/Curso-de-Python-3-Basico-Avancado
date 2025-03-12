7f97e6f007ea4ab3c5be370fb7a8aa850dae82da
Joao-Inacio <joaoinacio206@protonmail.com>
2025-03-12 14:25:38 -0300

    Função e Lista

--- Type annotations - Tipagem de dados/main.py
+++ Type annotations - Tipagem de dados/main.py
@@ -9,3 +9,15 @@ uma_lista: list = []
 uma_tupla: tuple = 1, 2, 3
 um_dic: dict = {}
 
+
+# Com função
+def soma(x: int, y: int, z: float) -> float:
+    return x + y + z
+
+print(soma(4, 5, 2))
+
+# Lista
+lista_int: list[int] = [1, 2, 3, 4]
+lista_string: list[str] = ["a", "b", "c"]
+lista_tupl: list[tuple] = [(1, "a"), (2, "b")]
+lista_lista_int: list[list[int]] = [[1], [4, 5]]
